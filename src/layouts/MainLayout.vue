0a851657e3ea66593934e04f7b484d13121d27a3
Deni Shaipov <deni.shaipp@gmail.com>
2023-07-05 21:21:12 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="text-center q-pa-lg text-h3 text-weight-light">
+      Turing Machine
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {},
+});
+</script>
